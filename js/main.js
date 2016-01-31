a149225576457e262070be5902b7e9655c80618d
Sriram Sundarraj <ssundarraj@gmail.com>
2016-02-01 00:30:11 +0530

    Added new default actions.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,80 @@
 document.addEventListener('DOMContentLoaded', populateDefaultSuggestions, false);
-function openNewTab(){
+
+function openNewTab() {
     chrome.tabs.create({});
-    console.log("TEST");
 }
+
+function openNewWindow() {
+    chrome.windows.create({});
+}
+
+function openHistory() {
+    chrome.tabs.create({url:"chrome://history"});
+}
+
+function openDownloads() {
+    chrome.tabs.create({url:"chrome://downloads"});
+}
+
+function openExtensions() {
+    chrome.tabs.create({url:"chrome://extensions"});
+}
+
+function openSettings() {
+    chrome.tabs.create({url:"chrome://settings"});
+}
+
+function closeCurrentTab() {
+    chrome.tabs.query({'active': true, 'windowId':chrome.windows.WINDOW_ID_CURRENT}, function (currentTabIds) {
+        var currentTabArray = [];
+        for (currentTabId of currentTabIds) {
+            currentTabArray.push(currentTabId.id);
+        }
+        chrome.tabs.remove(currentTabArray);
+    });
+}
+
+function reloadTab() {
+    chrome.tabs.reload();
+}
+
 var defaultSugestions = [
     {
         "text": "New Tab",
         "action": openNewTab
+    },
+    {
+        "text": "New Window",
+        "action": openNewWindow
+    },
+    {
+        "text": "Show History",
+        "action": openHistory
+    },
+    {
+        "text": "Show Downloads",
+        "action": openDownloads
+    },
+    {
+        "text": "Show Extensions",
+        "action": openExtensions
+    },
+    {
+        "text": "Show Settings",
+        "action": openSettings
+    },
+    {
+        "text": "Close Current Tab",
+        "action": closeCurrentTab
+    },
+    {
+        "text": "Reload Tab",
+        "action": reloadTab
     }
 ];
-function populateDefaultSuggestions(){
-    for(suggestion of defaultSugestions){
+
+function populateDefaultSuggestions() {
+    for (suggestion of defaultSugestions) {
         console.log(suggestion.text);
         var suggestionTag = document.createElement("li");
         suggestionTag.innerHTML = suggestion.text;
