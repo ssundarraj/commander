f5251473c3ec879c3e15dd0ec5cab282b8e55381
Sriram Sundarraj <ssundarraj@gmail.com>
2016-03-10 03:27:10 +0530

    Move to single JS file using Grunt.

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        src: ['js/actions.js', 'js/main.js', 'js/vendor/fuse.min.js'],
+        dest: 'js/dist/built.js',
+      },
+    },
+    uglify: {
+      options: {
+        mangle: false
+      },
+      my_target: {
+        files: {
+          'js/dist/output.min.js': ['js/actions.js', 'js/main.js']
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['uglify', 'concat']);
+}
